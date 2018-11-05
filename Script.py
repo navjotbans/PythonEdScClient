fe71b85ff6bc61c7f799836ef2750f11a249cf83
Navjot Bansal <navjotbansal09@gmail.com>
2018-11-05 22:23:22 +0530

    Small Alignment

--- Script.py
+++ Script.py
@@ -101,8 +101,8 @@ def Announcement(x,newpath):
                     download_file(currentPPT,currentName)
             os.chdir(scriptDirectory)    
 # url to the website
-Details = {'username':'f2016070@pilani.bits-pilani.ac.in',
-           'password':'bansalfamily007'}
+Details = {'username':'XXXX@pilani.bits-pilani.ac.in',
+           'password':'XXXX'}
 #creating a single session 
 try:
     session  = requests.session()
